a8bfb6ef51f713a7ebc9ad1125545017f6c08d85
Michael Andescavage <michaelandescavage@cloudfitsoftware.com>
2024-09-10 14:35:13 -0700

    initial commit from tutorial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+export default function Square() {
+    return (
+        <>
+          <button className="square">X</button>
+          <button className="square">X</button>
+          <button className="square">X</button>
+          <button className="square">X</button>
+          <button className="square">X</button>
+          <button className="square">X</button>
+          <button className="square">X</button>
+          <button className="square">X</button>
+          <button className="square">X</button>
+        </>
+      );
+  }
+  
